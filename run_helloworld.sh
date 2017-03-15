8b005844fa7a40c2458cfd21ca39724c07d68bfb
cloud chan <cloud.cck@gmail.com>
2017-03-15 13:39:36 +0800

    init

---
+++ run_helloworld.sh
@@ -0,0 +1,1 @@
+java -cp build/libs/scala-gradle.jar:build/libs/3rd-lib/scala-library-2.11.6.jar cck.cloud.HelloWorld
---
+++ src/main/scala/cck/cloud/HelloWorld.scala
@@ -0,0 +1,10 @@
+package cck.cloud
+
+/**
+  * Created by cloudchan on 15/03/2017.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
